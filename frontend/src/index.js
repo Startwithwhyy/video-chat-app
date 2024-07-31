166c8a1a64dc1051e2ec6c77256f0aa4d29be4d1
startwithwhyy <nuhaminzelalem7918@gmail.com>
2024-07-31 13:55:41 +0300

    created a person room

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// index.js or index.tsx
-import './index.css'; // Make sure this line is present
-
+import './App.css'; // Make sure this line is present
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,7 +12,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
